34f8e9c377c6c431844bbb71a52a28c0f13da4db
Nurina Junus <nurina.junus@gmail.com>
2022-02-16 12:14:13 -0700

    commit tree 84452ea1a71b7772a5f1d200a21de16eac774bf5

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,8 @@
 class Dog
-
+    def name=(dog_name) # INSTANCE method (in this case, SETS the name ("name equals" method), assigns arg (dog_name) to var (this_dogs_name)
+        @this_dogs_name = dog_name
+    end
+    def name # INSTANCE method (in this case, GETS the name)
+        @this_dogs_name
+    end
 end
